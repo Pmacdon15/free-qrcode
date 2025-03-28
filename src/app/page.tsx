7e3dfc7f1bac999a13fb4bbd460037682182c77d
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-27 18:00:07 -0600

    update css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,18 @@ export default function Home() {
 
   const handleGenerateQRCode = () => {
     if (inputRef.current) {
+      const value = inputRef.current.value;
+      if (!value.startsWith('http://') && !value.startsWith('https://')) {
+        inputRef.current.value = 'https://' + value;
+      }
       setQRCodeUrl(inputRef.current.value);
     }
   };
 
   return (
     <div className="flex flex-col justify-center items-center font-[family-name:var(--font-geist-sans)]">
-      <div className="flex flex-col items-center mt-24 p-4 gap-4 w-3/6 border">
+      <h1 className="flex flex-col items-center text-2xl mt-12 p-4 gap-4 w-5/6 md:w-3/6 border">Free Qr Code Generator</h1>
+      <div className="flex flex-col items-center mt-12 p-4 gap-4 w-5/6 md:w-3/6 border">
         <div className="flex gap-4 w-full ">
           <input 
             ref={inputRef} 
@@ -31,7 +36,7 @@ export default function Home() {
             Generate Qr Code
           </button>
         </div>
-        <QRCode value={`https://${qrCodeUrl}`} />
+        <QRCode value={`${qrCodeUrl}`} />
       </div>
     </div>
   );
